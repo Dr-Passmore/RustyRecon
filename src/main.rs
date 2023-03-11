f50712c0f09506af1905d0abf9b27b584a3a444a
Phillip Passmore <phillipspassmore@gmail.com>
2023-03-11 20:54:23 +0000

    scan_directory function added. Returns file names
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use rayon::prelude::*;
+use walkdir::WalkDir;
+
 fn main() {
     println!("Hello, world!");
+    let path = r#"C:\test"#;
+    scan_directory(path)
+
+}
+
+fn scan_directory(path: &str) {
+    WalkDir::new(path)
+    .into_iter()
+    .filter_map(Result::ok)
+    .par_bridge()
+    .for_each(|entry| {
+        let path = entry.path();
+        if path.is_file() {
+            println!("{}", path.display());
+        }
+    }) 
+    
 }
