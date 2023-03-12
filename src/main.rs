6bdfd72e0993d7523d4c617be0b4e62a4e190a61
Phillip Passmore <phillipspassmore@gmail.com>
2023-03-12 11:56:43 +0000

    Readable file size function added
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,28 @@ fn scan_directory(path: &str) {
             if let Ok(metadata) = fs::metadata(path) {
                 // add size
                 let size = metadata.len();
-                println!("{} ({} bytes)", path.display(),size);
-
+                println!("{} ({})", path.display(), size_readable_format(size));
             }
-            
-            
         }
     }) 
     
+}
+
+fn size_readable_format(size: u64) -> String {
+    const KB: u64 = 1024;
+    const MB: u64 = KB * 1024;
+    const GB: u64 = MB * 1024;
+    const TB: u64 = GB * 1024;
+
+    if size < KB {
+        format!("{} B", size)
+    } else if size < MB {
+        format!("{:.2} KB", size as f64 / KB as f64)
+    } else if size < GB {
+        format!("{:.2} MB", size as f64 / MB as f64)
+    } else if size < TB {
+        format!("{:.2} GB", size as f64 / GB as f64)
+    } else {
+        format!("{:.2} TB", size as f64 / TB as f64)
+    }
 }
